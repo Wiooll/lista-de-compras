978f980a8ea0eec590730279cd1cb19d37fd1f2b
Willian Sousa <141966506+Wiooll@users.noreply.github.com>
2024-05-15 10:03:45 -0300

    Delete SITE COMPRAS/backend directory

--- SITE COMPRAS/backend/config/db.js
+++
@@ -1,9 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(':memory:');
-
-db.serialize(() => {
-  db.run("CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)");
-  db.run("CREATE TABLE products (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price REAL, userId INTEGER)");
-});
-
-module.exports = db;

0b232f10987e6b27fcf2c1179b7dfcc5610b5a13
Willian Sousa <141966506+Wiooll@users.noreply.github.com>
2024-05-15 12:10:56 -0300

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(':memory:');
-
-db.serialize(() => {
-  db.run("CREATE TABLE usuarios (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, senha TEXT)");
-  db.run("CREATE TABLE produtos (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, preco REAL, usuarioId INTEGER)");
-});
-
-module.exports = db;
